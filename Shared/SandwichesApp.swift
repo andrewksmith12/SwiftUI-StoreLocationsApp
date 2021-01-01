b96206b26a8eebb8f6af24d80afe1f6e3f9a0dc6
Andrew K. Smith <andrew@andrewksmith.net>
2021-01-01 17:33:02 -0600

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Andrew Smith on 1/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
